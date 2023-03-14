ccd1f301b82e7f294de37ee6491fced742b20d11
yimewang <qwerttt7411@gmail.com>
2023-03-14 01:44:02 -0400

    completes exercise

---
+++ Coins/Coins.gd
@@ -0,0 +1,25 @@
+extends TileMap
+
+var coins = []
+const big_number = 1000000
+
+func _ready():
+	for x in range (1000):
+		for y in range (1000):
+			if get_cell(x, y) != -1:
+				coins.append(Vector2(x,y))
+	print(coins)
+
+func get_coin(p):
+	var coords = world_to_map(to_local(p))
+	print(coords)
+	var min_dist = big_number
+	var which_coin = Vector2.ZERO
+	for c in coins:
+		var d = coords.distance_to(c)
+		if d < min_dist:
+			min_dist = d
+			which_coin = c
+	if which_coin != Vector2.ZERO:
+		call_deferred("set_cellv", which_coin, -1)
+		update_dirty_quadrants()
